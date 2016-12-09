0068996bc7de8251a67d251224b6ea96f98a950c
Meenu Sundararaju <sundar22@msu.edu>
2016-12-09 15:46:45 -0500

    sync

--- Final Project/FinalProject.js
+++ Final Project/FinalProject.js
@@ -1,5 +1,5 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
+/*function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
         x.className += " responsive";
@@ -7,7 +7,7 @@ function myFunction() {
         x.className = "topnav";
     }
 }
-
+*/
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
